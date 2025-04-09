1b6d361ae2644ae6ef2df8274725855c62faad9e
Thien phuc <thienphuc484@gmail.com>
2025-04-09 10:55:04 +0700

    feat: add Naudio

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
 ﻿
+using NAudio.Wave;
+
 var musicFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
 var enumerable = Directory.EnumerateFiles(musicFolder, "*.mp3",  SearchOption.AllDirectories);
 
 var files = enumerable.ToList();
 
+var filePath = "";
 foreach (var file in files)
 {
-    Console.WriteLine(file);
+    filePath = file;
+}
+
+using var audioFile = new AudioFileReader(filePath);
+using var outputDevice = new WaveOutEvent();
+outputDevice.Init(audioFile);
+outputDevice.Volume = 0.25f;
+outputDevice.Play();
+while (outputDevice.PlaybackState == PlaybackState.Playing)
+{
+    Thread.Sleep(1000);
 }
