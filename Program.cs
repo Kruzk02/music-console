88d5c226c5a802b793bfbfbc0e7f9b552cbcdd9d
Thien phuc <thienphuc484@gmail.com>
2025-04-09 10:53:12 +0700

    Init commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+var musicFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+var enumerable = Directory.EnumerateFiles(musicFolder, "*.mp3",  SearchOption.AllDirectories);
+
+var files = enumerable.ToList();
+
+foreach (var file in files)
+{
+    Console.WriteLine(file);
+}
