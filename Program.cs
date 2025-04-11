467d243baf6d0f360bf9ef4f46db0fae2c30b543
Thien phuc <thienphuc484@gmail.com>
2025-04-11 09:12:32 +0700

    feat: add change volume and playback

--- Program.cs
+++ Program.cs
@@ -2,30 +2,61 @@
 using NAudio.Wave;
 using Spectre.Console;
 
-var filePath = AnsiConsole.Prompt(
+var musicFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+var musicFiles = Directory.EnumerateFiles(musicFolder, "*.mp3",  SearchOption.AllDirectories)
+    .Select(Path.GetFileName)
+    .Where(name => name is not null)
+    .Select(name => name!)
+    .ToList();
+
+var file = AnsiConsole.Prompt(
     new SelectionPrompt<string>()
         .Title("What music you want to play?")
         .PageSize(10)
         .MoreChoicesText("[grey](Move up and down to reveal more music)[/]")
-        .AddChoices(GetMusicFiles()));
+        .AddChoices(musicFiles));
 
-using var audioFile = new AudioFileReader(filePath);
+using var audioFile = new AudioFileReader(musicFolder + "/" + file);
 using var outputDevice = new WaveOutEvent();
+
 outputDevice.Init(audioFile);
-outputDevice.Volume = 0.25f;
+outputDevice.Volume = 1.0f;
 outputDevice.Play();
-while (outputDevice.PlaybackState == PlaybackState.Playing)
+
+while (true)
 {
-    Thread.Sleep(1000);
+    var key = Console.ReadKey(true).Key;
+    
+    HandleKey(key);
 }
 
-List<string> GetMusicFiles()
+void HandleKey(ConsoleKey key)
 {
-    var musicFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+    switch (key)
+    {
+        case ConsoleKey.Spacebar:
+            TogglePlayback();
+            break;
 
-    return Directory.EnumerateFiles(musicFolder, "*.mp3",  SearchOption.AllDirectories)
-        .Select(Path.GetFileNameWithoutExtension)
-        .Where(name => name is not null)
-        .Select(name => name!)
-        .ToList();
+        case ConsoleKey.UpArrow:
+            AdjustVolume(+0.1f);
+            break;
+
+        case ConsoleKey.DownArrow:
+            AdjustVolume(-0.1f);
+            break;
+    }
+}
+
+void TogglePlayback()
+{           
+    if (outputDevice.PlaybackState == PlaybackState.Playing)
+        outputDevice.Stop();
+    else
+        outputDevice.Play();
+}
+
+void AdjustVolume(float delta)
+{
+    outputDevice.Volume = Math.Clamp(outputDevice.Volume + delta, 0.0f, 1.0f);   
 }
