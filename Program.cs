eb5f5bfdffc7e54e2f502b6e29298ac7c2545994
Thien phuc <thienphuc484@gmail.com>
2025-04-15 09:13:28 +0700

    feat: add switch between music

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using NAudio.Wave;
 using Spectre.Console;
 
 var musicFolder = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+
 var musicFiles = Directory.EnumerateFiles(musicFolder, "*.mp3",  SearchOption.AllDirectories)
     .Select(Path.GetFileName)
     .Where(name => name is not null)
@@ -16,11 +17,12 @@ var file = AnsiConsole.Prompt(
         .MoreChoicesText("[grey](Move up and down to reveal more music)[/]")
         .AddChoices(musicFiles));
 
+var i = musicFiles.FindIndex(0, mFile => mFile == file);
+
 using var audioFile = new AudioFileReader(musicFolder + "/" + file);
 using var outputDevice = new WaveOutEvent();
 
 outputDevice.Init(audioFile);
-outputDevice.Volume = 1.0f;
 outputDevice.Play();
 
 while (true)
@@ -28,6 +30,22 @@ while (true)
     var key = Console.ReadKey(true).Key;
     
     HandleKey(key);
+    
+    var currentMusic = new TextPath(musicFolder + "/" + musicFiles[i])
+    {
+        RootStyle = new Style(Color.Red),
+        SeparatorStyle = new Style(Color.Green),
+        StemStyle = new Style(Color.Blue),
+        LeafStyle = new Style(Color.Yellow)
+    };
+
+    var table = new Table().AddColumn(new TableColumn(currentMusic)).Centered();
+
+    AnsiConsole.Live(table)
+        .Start(ctx =>
+        {
+            ctx.Refresh();
+        });
 }
 
 void HandleKey(ConsoleKey key)
@@ -45,7 +63,21 @@ void HandleKey(ConsoleKey key)
         case ConsoleKey.DownArrow:
             AdjustVolume(-0.1f);
             break;
+        
+        case ConsoleKey.LeftArrow:
+            outputDevice.Stop();
+            i -= 1;
+            outputDevice.Init(new AudioFileReader(musicFolder + "/" + musicFiles[i]));
+            outputDevice.Play();
+            break;
+        case ConsoleKey.RightArrow:
+            outputDevice.Stop();
+            i += 1;
+            outputDevice.Init(new AudioFileReader(musicFolder + "/" + musicFiles[i]));
+            outputDevice.Play();
+            break;
     }
+
 }
 
 void TogglePlayback()
